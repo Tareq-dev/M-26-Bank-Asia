1795b6bbc1b8df70219c7cdb494d601e28d5b137
Tarequl Islam <tareque.dev@gmail.com>
2022-02-11 20:14:45 +0600

    validation done

--- js/transaction.js
+++ js/transaction.js
@@ -1,37 +1,37 @@
-const depositBtn = document.getElementById('deposit-btn');
-depositBtn.addEventListener('click',function() {
-     // handle deposit
-     const inputDeposit = document.getElementById('input-deposit').value;
-     const inputNumber = parseFloat(inputDeposit);
-     //this function is called from below line no -
-     updateSpanText('currentDeposit', inputNumber);
-     document.getElementById('input-deposit').value = '';
-     // handle Balance and this function is called from below line no -26
-     updateSpanText('currentBalance', inputNumber);
-})
-const withdrawBtn = document.getElementById('withdraw-btn');
-withdrawBtn.addEventListener('click',function() {
-     console.log('click')
-     // handle withdraw
-     const inputWithdraw = document.getElementById('input-withdraw').value;
-     const inputNumber = parseFloat(inputWithdraw);
 
-     //this function is called from below line no -26
-     updateSpanText('currentWithdraw', inputNumber);
-     document.getElementById('input-withdraw').value = '';
-
-     // handle balance and this function is called from below line no -26
-     updateSpanText('currentBalance', -1*inputNumber);
-});
+ function getInputvalue (inputId) {
+     const inputField = document.getElementById(inputId).value;
+     const inputNumber = parseFloat(inputField);
+     // clear input
+     document.getElementById(inputId).value = '';
+     return inputNumber;
+}
 function updateSpanText(id ,inputNumber){
      const current = document.getElementById(id).innerText;
      const currentNumber = parseFloat(current);
      const totalBalance = currentNumber + inputNumber;
      document.getElementById(id).innerText = totalBalance;
      }
-// that function hard copy here
 
-      // const currentDeposit = document.getElementById('currentDeposit').innerText;
-     // const currentDepositNumber = parseFloat(currentDeposit);
-     // const totalDeposit = currentDepositNumber + inputDepositNumber;
-     // document.getElementById('currentDeposit').innerText = totalDeposit;
+const depositBtn = document.getElementById('deposit-btn').addEventListener('click',function() {
+     // handle deposit
+     const inputNumber = getInputvalue("input-deposit");
+     if(inputNumber>0){
+          updateSpanText('currentDeposit', inputNumber);
+          updateSpanText('currentBalance', inputNumber);
+     }
+})
+const withdrawBtn = document.getElementById('withdraw-btn').addEventListener('click',function() {
+     // handle withdraw
+     const inputNumber = getInputvalue("input-withdraw");
+     const currentBalance = document.getElementById("currentBalance").innerText;
+     const currentBalanceNumber = parseFloat(currentBalance);
+
+   if(inputNumber > 0 && inputNumber < currentBalanceNumber ){
+     updateSpanText('currentWithdraw', inputNumber);
+     updateSpanText('currentBalance', -1* inputNumber);
+   }
+   if(inputNumber > currentBalanceNumber ){
+     alert('You can not withdraw more than what you have in your account.');   
+   }
+});
